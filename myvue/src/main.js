2e80f825a9fa4d7262556db674e17409ecc07e69
ma20010428 <1990118782@qq.com>
2021-05-24 19:45:00 +0800

    element-ui

--- myvue/src/main.js
+++ myvue/src/main.js
@@ -2,5 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from 'element-ui';
+
+
+import Vue from 'vue';
+
+Vue.use(ElementUI);
 
 createApp(App).use(store).use(router).mount('#app')
--- myvue/src/views/About.vue
+++ myvue/src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-time-picker
+    v-model="value1"
+    :picker-options="{
+      selectableRange: '18:30:00 - 20:30:00'
+    }"
+    placeholder="任意时间点">
+    </el-time-picker>
+    <el-time-picker
+      arrow-control
+      v-model="value2"
+      :picker-options="{
+        selectableRange: '18:30:00 - 20:30:00'
+      }"
+      placeholder="任意时间点">
+    </el-time-picker>
   </div>
 </template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        value1: new Date(2016, 9, 10, 18, 40),
+        value2: new Date(2016, 9, 10, 18, 40)
+      };
+    }
+  }
+</script>
+
+
+
